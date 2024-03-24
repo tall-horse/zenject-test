f0d3621e821d5a24d23a0b840bc7982977d1844a
tall-horse <54848630+tall-horse@users.noreply.github.com>
2024-03-24 19:15:24 +0100

    improvement of GenerateMapBlueprint method logic

--- AStar/Assets/Scripts/TileMap.cs
+++ AStar/Assets/Scripts/TileMap.cs
@@ -13,7 +13,7 @@ public class TileMap : MonoBehaviour
   private const float OffsetX = -0.5f;
   private const float OffsetZ = -0.26f;
   [SerializeField] private List<GameObject> tilePrefabs = new(5);
-  private Dictionary<(int, int), int> biomeData = new Dictionary<(int, int), int>();
+  private Dictionary<(int, int), int> biomeData;
   // Start is called before the first frame update
   void Start()
   {
@@ -21,86 +21,82 @@ public class TileMap : MonoBehaviour
     SpawnTiles();
   }
   private void GenerateMapBlueprint()
-  {
+{
     tilesCollection = new List<Tile>();
 
-    List<KeyValuePair<(int, int), int>> itemsToAdd = new()
+    biomeData = new Dictionary<(int, int), int>
     {
-    new((2, 2), 1),
-    new((7, 3), 1),
-    new((6, 4), 1),
-    new((0, 4), 1),
-    new((7, 4), 1),
-    new((0, 5), 1),
-    new((2, 5), 1),
-    new((6, 5), 1),
-    new((0, 6), 1),
-    new((1, 6), 1),
-    new((2, 6), 1),
-    new((0, 7), 1),
-    new((1, 7), 1),
+        {(2, 2), 1},
+        {(7, 3), 1},
+        {(6, 4), 1},
+        {(0, 4), 1},
+        {(7, 4), 1},
+        {(0, 5), 1},
+        {(2, 5), 1},
+        {(6, 5), 1},
+        {(0, 6), 1},
+        {(1, 6), 1},
+        {(2, 6), 1},
+        {(0, 7), 1},
+        {(1, 7), 1},
 
-    new((3, 0), 0),
-    new((5, 0), 0),
-    new((1, 1), 0),
-    new((3, 1), 0),
-    new((4, 1), 0),
-    new((0, 2), 0),
-    new((1, 2), 0),
-    new((3, 3), 0),
-    new((5, 3), 0),
-    new((1, 4), 0),
-    new((3, 4), 0),
-    new((5, 4), 0),
-    new((1, 5), 0),
-    new((3, 6), 0),
-    new((5, 6), 0),
-    new((7, 6), 0),
+        {(3, 0), 0},
+        {(5, 0), 0},
+        {(1, 1), 0},
+        {(3, 1), 0},
+        {(4, 1), 0},
+        {(0, 2), 0},
+        {(1, 2), 0},
+        {(3, 3), 0},
+        {(5, 3), 0},
+        {(1, 4), 0},
+        {(3, 4), 0},
+        {(5, 4), 0},
+        {(1, 5), 0},
+        {(3, 6), 0},
+        {(5, 6), 0},
+        {(7, 6), 0},
 
-    new((0, 0), 2),
-    new((1, 0), 2),
-    new((2, 0), 2),
-    new((2, 1), 2),
-    new((4, 3), 2),
-    new((4, 4), 2),
-    new((5, 5), 2),
-    new((6, 6), 2),
-    new((5, 7), 2),
-    new((6, 7), 2),
+        {(0, 0), 2},
+        {(1, 0), 2},
+        {(2, 0), 2},
+        {(2, 1), 2},
+        {(4, 3), 2},
+        {(4, 4), 2},
+        {(5, 5), 2},
+        {(6, 6), 2},
+        {(5, 7), 2},
+        {(6, 7), 2},
 
-    new((7, 0), 4),
-    new((6, 1), 4),
-    new((7, 1), 4),
-    new((3, 7), 4),
-    new((4, 2), 4),
-    new((5, 2), 4),
-    new((7, 2), 4),
-    new((0, 3), 4),
-    new((1, 3), 4),
-    new((2, 3), 4),
-    new((2, 4), 4)
-};
-foreach (var item in itemsToAdd)
-{
-  biomeData.Add(item.Key, item.Value);
-}
+        {(7, 0), 4},
+        {(6, 1), 4},
+        {(7, 1), 4},
+        {(3, 7), 4},
+        {(4, 2), 4},
+        {(5, 2), 4},
+        {(7, 2), 4},
+        {(0, 3), 4},
+        {(1, 3), 4},
+        {(2, 3), 4},
+        {(2, 4), 4}
+    };
 
-    //populate map data according to the image in a task document
+    //populate map data according to the biomeData
     for (int x = 0; x < MapSizeX; x++)
     {
-      for (int z = 0; z < MapSizeZ; z++)
-      {
-        if (biomeData.ContainsKey((x, z)))
+        for (int z = 0; z < MapSizeZ; z++)
         {
-          mapData.Add((x, z), biomeData[(x, z)]);
+            if (biomeData.ContainsKey((x, z)))
+            {
+                mapData.Add((x, z), biomeData[(x, z)]);
+            }
+            else
+            {
+                mapData.Add((x, z), 0); // Default to empty tile
+            }
         }
-        else
-        {
-          mapData.Add((x, z), 0); // Default to empty tile
-        }
-      }
     }
-  }
+}
   private void SpawnTiles()
   {
     for (int x = 0; x < MapSizeX; x++)
